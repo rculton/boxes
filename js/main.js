68697ee58712d55c78af63142ebb57623d73becc
Ryan Culton <rculton@gmail.com>
2017-08-28 10:05:45 -0700

    initial commit / created foundation

---
+++ js/main.js
@@ -0,0 +1,21 @@
+console.log("javascript is loaded")
+
+function sayHello(name){
+    return "Hello " + (name || "anonymous person")
+}
+
+function setH1(string){
+    var h1 = document.querySelector('h1')
+    h1.innerText = string
+
+}
+
+function showGreeting(){
+    setH1(sayHello("Ryan"))
+}
+
+var button = document.querySelector('button')
+
+button.addEventListener('click', function(){
+    setH1(sayHello("BANANA!"))
+})
