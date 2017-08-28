76c390f462910b7b34ab7d4fde3f24ec1dd997d7
Ryan Culton <rculton@gmail.com>
2017-08-28 10:34:16 -0700

    Fixed querySelector statement

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function sayHello(name){
 }
 
 function setH1(string){
-    var h1 = document.querySelector('h1')[1]
+    var h1 = document.querySelectorAll('h1')[1]
     h1.innerText = string
 
 }
@@ -17,5 +17,5 @@ function showGreeting(){
 var button = document.querySelector('button')
 
 button.addEventListener('click', function(){
-    setH1(sayHello("BANANA!"))
+    setH1(sayHello("Ryan!"))
 })
